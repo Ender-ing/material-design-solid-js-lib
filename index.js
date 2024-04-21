8d67cba76e4909ce56d0bdee9ef7da0bc9566176
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-04-21 18:13:24 +0300

    Update files

--- index.js
+++ index.js
@@ -27,51 +27,66 @@ function loadScript(src, onload, onerror){
 }
 
 const resourceList = [
-["elevated-button", "@material/button--elevated-button.js"],
-["filled-button", "@material/button--filled-button.js"],
-["filled-tonal-button", "@material/button--filled-tonal-button.js"],
-["outlined-button", "@material/button--outlined-button.js"],
-["text-button", "@material/button--text-button.js"],
+["elevated-button", "@material/buttons.js"],
+["filled-button", "@material/buttons.js"],
+["filled-tonal-button", "@material/buttons.js"],
+["outlined-button", "@material/buttons.js"],
+["text-button", "@material/buttons.js"],
 ["checkbox", "@material/checkbox.js"],
-["chip-set", "@material/chips--chip-set.js"],
-["assist-chip", "@material/chips--assist-chip.js"],
-["filter-chip", "@material/chips--filter-chip.js"],
-["input-chip", "@material/chips--input-chip.js"],
-["suggestion-chip", "@material/chips--suggestion-chip.js"],
+["chip-set", "@material/chips.js"],
+["assist-chip", "@material/chips.js"],
+["filter-chip", "@material/chips.js"],
+["input-chip", "@material/chips.js"],
+["suggestion-chip", "@material/chips.js"],
 ["dialog", "@material/dialog.js"],
 ["divider", "@material/divider.js"],
 ["elevation", "@material/elevation.js"],
-["fab", "@material/fab--fab.js"],
-["branded-fab", "@material/fab--branded-fab.js"],
-["filled-field", "@material/field--filled-field.js"],
-["outlined-field", "@material/field--outlined-field.js"],
+["fab", "@material/fabs.js"],
+["branded-fab", "@material/fabs.js"],
+["filled-field", "@material/fields.js"],
+["outlined-field", "@material/fields.js"],
 ["focus-ring", "@material/focus.js"],
 ["icon", "@material/icon.js"],
-["icon-button", "@material/iconbutton--icon-button.js"],
-["filled-icon-button", "@material/iconbutton--filled-icon-button.js"],
-["filled-tonal-icon-button", "@material/iconbutton--filled-tonal-icon-button.js"],
-["outlined-icon-button", "@material/iconbutton--outlined-icon-button.js"],
-["list", "@material/list--list.js"],
-["list-item", "@material/list--list-item.js"],
-["menu", "@material/menu--menu.js"],
-["menu-item", "@material/menu--menu-item.js"],
-["sub-menu", "@material/menu--sub-menu.js"],
-["linear-progress", "@material/progress--linear-progress.js"],
-["circular-progress", "@material/progress--circular-progress.js"],
+["icon-button", "@material/iconbuttons.js"],
+["filled-icon-button", "@material/iconbuttons.js"],
+["filled-tonal-icon-button", "@material/iconbuttons.js"],
+["outlined-icon-button", "@material/iconbuttons.js"],
+["list", "@material/lists.js"],
+["list-item", "@material/lists.js"],
+["menu", "@material/menus.js"],
+["menu-item", "@material/menus.js"],
+["sub-menu", "@material/menus.js"],
+["linear-progress", "@material/progress.js"],
+["circular-progress", "@material/progress.js"],
 ["radio", "@material/radio.js"],
 ["ripple", "@material/ripple.js"],
-["filled-select", "@material/select--filled-select.js"],
-["outlined-select", "@material/select--outlined-select.js"],
-["select-option", "@material/select--select-option.js"],
+["filled-select", "@material/selects.js"],
+["outlined-select", "@material/selects.js"],
+["select-option", "@material/selects.js"],
 ["slider", "@material/slider.js"],
 ["switch", "@material/switch.js"],
-["tabs", "@material/tabs--tabs.js"],
-["primary-tab", "@material/tabs--primary-tab.js"],
-["secondary-tab", "@material/tabs--secondary-tab.js"],
-["filled-text-field", "@material/textfield--filled-text-field.js"],
-["outlined-text-field", "@material/textfield--outlined-text-field.js"]
+["tabs", "@material/tabs.js"],
+["primary-tab", "@material/tabs.js"],
+["secondary-tab", "@material/tabs.js"],
+["filled-text-field", "@material/textfields.js"],
+["outlined-text-field", "@material/textfields.js"]
 // Elements not part of the material design web components library should start with "x-" (e.g. x-layout)
 ], flatResourceList = resourceList.flat(Infinity);
+
+// Collect all used tags inside an XMLDocument
+function getXDOCTags(xmlDoc) {
+    let allTags = new Set();
+
+    const collectTags = (node) => {
+        allTags.add(node.nodeName);
+        for (const child of node.childNodes) {
+            collectTags(child);
+        }
+    };
+    collectTags(xmlDoc.documentElement); // Start from the root element
+
+    return Array.from(allTags);
+}
 
 // Process .design XML data and load needed resources
 // Input: xmlDoc <XMLDocument>, callback <Function>, onerror <Function>, ignore <Array>
@@ -83,18 +98,20 @@ export function processDesign(xmlDoc, callback, onerror, ignore = []){
     let neededResources = null, loadedResources = 0,
     usedElements = [];
     // Get the number of needed resources
-    var tags = xmlDoc.getElementsByTagName('*');
+    var tags = getXDOCTags(xmlDoc);
+    console.log(window.tags = tags)
     for (var i = 0; i < tags.length; i++) {
         // ~ Exclude elements on [ignore] list! ~
-        if((!usedElements.includes(tags[i].nodeName)) && (!ignore.includes(tags[i].nodeName))){
+        if((!usedElements.includes(tags[i])) && (!ignore.includes(tags[i]))){
             // Check if this element needs any resources to be loaded
-            if(flatResourceList.includes(tags[i].nodeName)){
-                usedElements.push(tags[i].nodeName);
+            if(flatResourceList.includes(tags[i])){
+                usedElements.push(tags[i]);
             }
         }
     }
     tags = null;
     // Update resource tracking info
+    console.log(window.usedElements = usedElements);
     neededResources = usedElements.length;
     callback(neededResources, loadedResources);
 
